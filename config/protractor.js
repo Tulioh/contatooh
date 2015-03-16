c21a65ae6257357dd8b583e6a4cb33d76a3a8400
Tulioh <tuliohdvp@gmail.com>
2015-03-16 10:54:04 -0300

    Configuração do travis pronta

--- config/protractor.js
+++ config/protractor.js
@@ -1,8 +1,16 @@
 var config = require( "./config" )();
 
 exports.config = {
-    specs : [ "../test/e2e/**/*.js" ],
+    sauceUser : config.sauceUser,
+    sauceKey : config.sauceKey,
+    capabilities : {
+        'name': config.sauceTestName,
+        'browserName': 'chrome',
+        'tunnel-identifier': config.travisJobNumber,
+        'build': config.travisBuild
+    },
 
+    specs : [ "../test/e2e/**/*.js" ],
     onPrepare : function() {
         browser.driver.get( "http://localhost:3000/#/auth" );
         browser.driver.findElement( by.id("entrar") ).click();
